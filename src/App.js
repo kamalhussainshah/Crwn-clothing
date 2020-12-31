5639d44f132c451bb9f48787ad268b3768714af1
kamalshah205@gmail.com <you@example.com>
2020-12-31 18:55:29 +0500

    header...

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
-const HatsPage = () => (
-  <div>
-    <h1> HATS PAGE </h1>
-  </div> 
-);
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
-    </Switch>
+    <div>
+      <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+        </Switch>
+    </div>
   );
 }
 
